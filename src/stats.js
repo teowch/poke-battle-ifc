92db611c37671299319f3d94e93b9c0367a29549
Teo <teovs.wch@gmail.com>
2020-12-14 04:37:07 -0300

    Update

--- src/stats.js
+++ src/stats.js
@@ -4,7 +4,12 @@ const Battle = require('../pokemon-battle');
 
 function battle(req, res) {
     // console.log(JSON.parse(req.query.leader))
-    console.log('called')
+    if (!req.query.user || !req.query.leader) {
+        document.write('Faça uma requisição com JSON');
+        console.log('Faça uma requisição com JSON');
+        res.send('Faça uma requisição com JSON');
+        return
+    }
     let match = Battle.battle(JSON.parse(req.query.user), JSON.parse(req.query.leader));
 
     res.send(match)
