523ba50f8bc48c046b1e6775f9abbc7085c4bf86
Teo <teovs.wch@gmail.com>
2020-12-14 04:22:36 -0300

    Update

--- src/stats.js
+++ src/stats.js
@@ -4,6 +4,7 @@ const Battle = require('../pokemon-battle');
 
 function battle(req, res) {
     // console.log(JSON.parse(req.query.leader))
+    console.log('called')
     let match = Battle.battle(JSON.parse(req.query.user), JSON.parse(req.query.leader));
 
     res.send(match)
