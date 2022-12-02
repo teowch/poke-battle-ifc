c4f9bd3b9b5dacced8d4251a13d5dccd3a510a59
Teo <teovs.wch@gmail.com>
2022-12-02 01:21:25 -0300

    Format

--- src/stats.js
+++ src/stats.js
@@ -1,17 +1,20 @@
-require('coffee-script/register');
+require('coffee-script/register')
 
-const Battle = require('../pokemon-battle');
+const Battle = require('../pokemon-battle')
 
 function battle(req, res) {
-    // console.log(JSON.parse(req.query.leader))
-    if (!req.query.user || !req.query.leader) {
-        console.log('Faça uma requisição com JSON');
-        res.send('Faça uma requisição com JSON');
-        return
-    }
-    let match = Battle.battle(JSON.parse(req.query.user), JSON.parse(req.query.leader));
+	console.log(req.query.user)
+	if (!req.query.user || !req.query.leader) {
+		console.log('Faça uma requisição com JSON')
+		res.send('Faça uma requisição com JSON')
+		return
+	}
+	let match = Battle.battle(
+		JSON.parse(req.query.user),
+		JSON.parse(req.query.leader)
+	)
 
-    res.send(match)
+	res.send(match)
 }
 
-module.exports = battle;
+module.exports = battle
